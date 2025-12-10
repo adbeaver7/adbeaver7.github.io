286eab98f0ec7f28d6d12da62654cda2521e89f0
seo-bo <fgxhmd@kumoh.ac.kr>
2025-12-10 14:50:41 +0900

    Deploy Flutter web

--- android/app/src/main/kotlin/com/example/adbeaver/MainActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.adbeaver
-
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity()
--- run_chrome.sh
+++
@@ -1,6 +0,0 @@
-clear
-clear
-clear
-clear
-clear
-flutter run -d chrome
